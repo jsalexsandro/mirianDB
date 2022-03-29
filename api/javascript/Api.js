30653d61a1b0887a8344600215ee502c7dc873f4
eualexdev <eujosealexsandro@gmail.com>
2022-03-28 21:56:07 -0300

    commit final removi bugs e possiveis erros

--- api/javascript/Api.js
+++ api/javascript/Api.js
@@ -1,13 +1,17 @@
 class MirianDB {
   constructor(url){
     if (url !== void 0){
-      this.url = url
+      this.url = String(url)
+      const existBar = (this.url.slice(this.url.length-1,this.url.length) == "/" ? true :false)
+      if (existBar == true){
+        this.url = String(this.url).slice(0,this.url.length-1)
+      }
     }
   }
 
   read(calback){
     if (calback !== void 0)
-      fetch(this.url+"/read-database").then(
+      fetch(this.url+"/read-database",{method:"GET"}).then(
         (data) => {
           return data.json()
         }
@@ -18,12 +22,14 @@ class MirianDB {
         } else {
           throw Error("Not Possible Get Values")
         }
+      }).catch(() => {
+        throw Error("ERROR IN TRY FETCH")
       })
   }
 
   write(object,calback){
     if (object !== void 0 && calback !== void 0)
-      fetch(this.url+`/write-database?value=${JSON.stringify(object)}`).then(
+      fetch(this.url+`/write-database/?value=${JSON.stringify(object)}`,{method:"GET"}).then(
         (data) => {
           return data.json()
         }
@@ -33,6 +39,8 @@ class MirianDB {
         } else {
           throw Error("Not Possible Write Values")
         }
+      }).catch(() => {
+        throw Error("ERROR IN TRY FETCH")
       })
   }
 }
--- src/server/__init__.py
+++ src/server/__init__.py
@@ -4,12 +4,14 @@ import configparser
 import os
 import json
 import sys
+import colorama
 
 from src.less.Database import Database
 
 def MirianDbServer():
   try:
     ini = open(os.path.join(os.getcwd(),".mirian/props"),"rt",encoding="utf-8").read()
+    colorama.init()
   except:
     print("Root not configured!\nRun for Register root:\n $ mirian -r")
     sys.exit()
@@ -24,18 +26,18 @@ def MirianDbServer():
   def index():
     return ""
 
-  @app.route("/@"+rootName)
+  @app.route("/@"+rootName,methods=["GET"])
   def func1():
     return "" 
 
-  @app.route(f"/@{rootName}/read-database")
+  @app.route(f"/@{rootName}/read-database",methods=["GET"])
   def func2():
     try:
       return json.dumps({"error":False,"values":Database.read(passName)})
     except:
       return json.dumps({"error":True})
 
-  @app.route(f"/@{rootName}/write-database/")
+  @app.route(f"/@{rootName}/write-database/",methods=["GET"])
   def func3():
     write = request.args.get("value",default = "[]")
     print(write)
